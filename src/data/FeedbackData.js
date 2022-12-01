2829dcf4fbdd36a310d2c878fcf0ec8243ae7db5
amex23 <randelb2022@gmail.com>
2022-12-02 01:22:47 +0800

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio voluptate eius, sit eligendi nam pariatur beatae fugiat suscipit facilis, ducimus animi, velit provident nisi nostrum inventore. Aut, itaque quidem!",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio voluptate eius, sit eligendi nam pariatur beatae fugiat suscipit facilis, ducimus animi, velit provident nisi nostrum inventore. Aut, itaque quidem!",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio voluptate eius, sit eligendi nam pariatur beatae fugiat suscipit facilis, ducimus animi, velit provident nisi nostrum inventore. Aut, itaque quidem!",
+  },
+];
+
+export default FeedbackData;
