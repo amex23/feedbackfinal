ef43914826d1d2a54ba9295f195dfeaee371bb17
amex23 <randelb2022@gmail.com>
2022-12-04 16:59:42 +0800

    new update

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -4,13 +4,14 @@ import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
-
-  return (
+  return isLoading ? (
+    <h3>Loading...</h3>
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
